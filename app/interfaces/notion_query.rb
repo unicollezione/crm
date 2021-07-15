f2bdd2cad82a293e2ffc2bcc4a80d2b79d2ad735
Gluuck <ostapenkov74@gmail.com>
2021-07-14 23:15:21 -0700

    change notiontquery & gitignor ervnc

--- app/interfaces/notion_query.rb
+++ app/interfaces/notion_query.rb
@@ -1,6 +1,12 @@
-class FindNotionQuery
+require 'notion_api'
+
+class NotionQuery < AbstractQuery 
 
 	def find_notion_query
+
+	 @client = NotionAPI::Client.new("ENV['NOTION_API_TOKEN']")
+
+	
 		query = NotionQuery.find(query{order(id: args)})
 	end
 end
--- spec/factories/customer.rb
+++ spec/factories/customer.rb
@@ -1,24 +1,7 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :customer do
-    nickname {Faker::Name.first_name}
-    idx {Faker::IDNumber.valid}
-
-    factory :customer_with_products do
-      transient do
-        products_count {5}
-      end
-      after(:create) do |customer,e|
-        create_list(:product, e.products_count, customer: customer)
-      end
-    end
-
-    #factory :customer_with_orders do
-     # transient do
-      #  orders_count {5}
-      #end
-      #after(:create) do |customer,e|
-      #  create_list(:order, e.orders_count, customer: customer)
-      #end
-    #end
+    sequence(:nickname) { |n| "factorycustomer#{n}" }
   end
 end
