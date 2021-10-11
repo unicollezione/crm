7c70eed30e699936d4d1eca6e154ad538d0fc768
AlexIv <alx.ekb@gmail.com>
2021-10-11 14:47:03 +0500

    Fix webpacker deployment #7

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@
 import Rails from '@rails/ujs'
 import * as ActiveStorage from '@rails/activestorage'
 // import 'channels'
-import 'application.scss'
+import 'stylesheets/application.scss'
 Rails.start()
 ActiveStorage.start()
 
