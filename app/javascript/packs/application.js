a100d522f1543e6ef6abc2740a722e9f65d0c065
AlexIv <alx.ekb@gmail.com>
2021-10-11 14:03:34 +0500

    Fix webpacker deployment #2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,16 @@
 // Support component names relative to this directory:
-var componentRequireContext = require.context("components", true);
-var ReactRailsUJS = require("react_ujs");
-ReactRailsUJS.useContext(componentRequireContext);
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from '@rails/ujs'
+import * as ActiveStorage from '@rails/activestorage'
+import 'channels'
+import 'application.scss'
+Rails.start()
+ActiveStorage.start()
+
+const componentRequireContext = require.context('components', true)
+const ReactRailsUJS = require('react_ujs')
+ReactRailsUJS.useContext(componentRequireContext)
