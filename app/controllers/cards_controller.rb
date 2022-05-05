07bdb8fe8b8d87ac4963fc55b77e7469d6f086b4
Gluuck <ostapenkov74@gmail.com>
2022-05-04 22:24:12 -0700

    change find list & add file path

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,7 +2,7 @@
 # CardsController
 # Render cards
 class CardsController < ApplicationController
-  
+
   before_action :find_card , only: %i[show trello image file_path]
 
   def index
@@ -28,7 +28,7 @@ class CardsController < ApplicationController
 
   def image
     pdf = MiniMagick::Image.open([ENV['TEMPORARY_ASSETS_PATH'], "#{@card.idx}"].join)
-    pdf.format "jpg"
+    pdf.format("jpg")
     pdf.write([path, "#{@card.idx}.jpg"].join)
     @card.illustration.attach(**trello_card_attributes)
   end
