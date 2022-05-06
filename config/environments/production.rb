d012f4081706624a3d87734b78b3038a029fba3c
Alex Smirnov <sas2job@gmail.com>
2022-05-06 22:14:44 +0300

    Add active storage to symbol

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   # config.active_storage.service = :local
-  config.active_storage.service = ENV['ACTIVE_STORAGE_SERVICE']
+  config.active_storage.service = ENV['ACTIVE_STORAGE_SERVICE'].to_sym
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
