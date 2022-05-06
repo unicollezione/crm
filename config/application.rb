b8090f0f516df8523b6cce63bb84037527377b87
Alex Smirnov <sas2job@gmail.com>
2022-05-06 15:29:28 +0300

    Fix Zeitwerk error

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,10 @@ Bundler.require(*Rails.groups)
 module Crm
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    # config.load_defaults 6.0
+    # if used config.load_defaults 6.0 then received message error `to define constant ApplicationCable::Channel, but didn't (Zeitwerk::NameError)`
+
+    config.autoload = :classic
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
