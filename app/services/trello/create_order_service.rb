d5f0044332d12b828261d47943be19715e10326b
Gluuck <ostapenkov74@gmail.com>
2022-05-23 01:37:27 -0700

    create services pdf & jpg

--- app/services/trello/create_order_service.rb
+++ app/services/trello/create_order_service.rb
@@ -58,7 +58,6 @@ module Trello
     attr_reader :order
 
     def send_order_to_trello
-      generate_jpg
       send_attachment
     end
 
@@ -66,73 +65,10 @@ module Trello
       @card ||= Trello::Card.create(name: "##{order.idx}", list_id: order.workroom.trello_list.list)
     end
 
-    def jpg_name
-      "#{order.idx}.jpg"
-    end
-
-    def path_for(name)
-      Rails.root.join(path, name)
-    end
-
-    def trello_card_attributes
-      {
-        io: File.open(path_for(jpg_name)),
-        filename: "#{order.idx}.jpg",
-        content_type: 'image/jpg'
-      }
-    end
-
-    def path
-      path = "./tmp/#{self.class.to_s.downcase}/"
-      Dir.mkdir(path) unless Dir.exist?(path)
-      path
-    end
-
-    def pdf_name
-      "#{order.idx}.pdf"
-    end
-
-    def pdf_html
-      ApplicationController.render(template: 'cards/trello',
-                                   assigns: {
-                                     pdf: pdf_name,
-                                     print_media_type: true,
-                                     orientation: 'Portrait',
-                                     page_size: 'A4'
-                                   },
-                                   encoding: 'utf8',
-                                   locals: { :@card => order })
-    end
-
-    def wicked_pdf
-      WickedPdf.new.pdf_from_string(pdf_html)
-    end
-
-    def render_pdf
-      return if order.trello_pdf.attached?
-
-      File.open(path_for(pdf_name), 'wb') { |file| file << wicked_pdf }
-
-      order.trello_pdf.attach(io: File.open(path_for(pdf_name)),
-                              filename: pdf_name)
-    end
-
-    def generate_jpg
-      return if order.illustration.attached?
-
-      render_pdf
-
-      pdf = MiniMagick::Image.open(path_for(pdf_name))
-      pdf.format('jpg')
-      pdf.write(path_for(jpg_name))
-
-      order.illustration.attach(**trello_card_attributes)
-    end
-
     def send_attachment
       attachment = Trello::Card
                    .find(card.id)
-                   .add_attachment(File.open(path_for(jpg_name), 'r'),
+                   .add_attachment(File.open(TrelloJpg.new(order).call, 'r'),
                                    jpg_name)
 
       attachment_id = JSON.parse(attachment.body)['id']
