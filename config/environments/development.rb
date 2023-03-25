75b7aa90faca4d7adb6c0c9f5e9d8036ca2d6f52
AlexIv <alx.ekb@gmail.com>
2023-03-26 00:52:20 +0100

    Tweak dev environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,10 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.asset_host = 'http://localhost:3000'
+
+  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+
+  config.sass.inline_source_maps = true
 end
